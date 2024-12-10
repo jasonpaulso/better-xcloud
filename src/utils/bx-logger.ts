597cc9782d1198e6bb24044e9e6b780daf860771
redphx <96280+redphx@users.noreply.github.com>
2024-12-10 20:51:00 +0700

    Always show error log

--- src/utils/bx-logger.ts
+++ src/utils/bx-logger.ts
@@ -7,12 +7,12 @@ const enum TextColor {
 }
 
 export class BxLogger {
-    static info = (tag: string, ...args: any[]) => BxLogger.log(TextColor.INFO, tag, ...args);
-    static warning = (tag: string, ...args: any[]) => BxLogger.log(TextColor.WARNING, tag, ...args);
+    static info = (tag: string, ...args: any[]) => BX_FLAGS.Debug && BxLogger.log(TextColor.INFO, tag, ...args);
+    static warning = (tag: string, ...args: any[]) => BX_FLAGS.Debug && BxLogger.log(TextColor.WARNING, tag, ...args);
     static error = (tag: string, ...args: any[]) => BxLogger.log(TextColor.ERROR, tag, ...args);
 
     private static log(color: string, tag: string, ...args: any) {
-        BX_FLAGS.Debug && console.log(`%c[BxC]`, `color:${color};font-weight:bold;`, tag, '//', ...args);
+        console.log(`%c[BxC]`, `color:${color};font-weight:bold;`, tag, '//', ...args);
     }
 }
 
